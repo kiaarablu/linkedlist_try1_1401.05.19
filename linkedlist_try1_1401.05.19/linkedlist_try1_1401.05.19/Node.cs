bc573b9510319d74d49312cb16a155056496e7ca
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-08-11 14:29:26 +0430

    Add files via upload

---
+++ linkedlist_try1_1401.05.19/linkedlist_try1_1401.05.19/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist_try1_1401._05._19
+{
+    public class Node
+    {
+        public Node next { get; set; }
+        public int value { get; set; }
+    }
+    public class Amaliat
+    {
+        Node firstnode = new Node();
+        Node lastnode = new Node();
+
+        public Amaliat()
+        {
+            firstnode.value = 1;
+            firstnode.next = null;
+            lastnode = firstnode;
+        }
+        public void Push(int val)
+        {
+            Node x = new Node();
+            x.value = val;
+            lastnode.next = x;
+            lastnode = x;
+        }
+        public void print()
+        {
+            Node y = firstnode;
+            while (y != null)
+            {
+                Console.WriteLine(y.value);
+                y = y.next;
+            }
+
+        }
+
+
+    }
+}
---
+++ linkedlist_try1_1401.05.19/linkedlist_try1_1401.05.19/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedlist_try1_1401._05._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Amaliat x = new Amaliat();
+            if (a == "s")
+            {
+                x.Push(10);
+                x.Push(11);
+                x.Push(12);
+                x.Push(13);
+                x.Push(14);
+                x.Push(15);
+                x.Push(16);
+                x.Push(17);
+                x.Push(18);
+                x.Push(19);
+                x.Push(20);
+                x.Push(21);
+                x.Push(22);
+                x.Push(23);
+                x.Push(24);
+                x.Push(25);
+                x.Push(26);
+                x.Push(27);
+                x.print();
+            }
+            else
+            {
+                Console.WriteLine("password not found...!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
